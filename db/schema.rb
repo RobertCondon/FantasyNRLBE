cdadf8f1a3374e8400c613ef289c6d72800cd81d
RobertCondon <robert.condon@soeidental.com>
2024-01-02 12:23:06 +1300

    ?

--- db/schema.rb
+++ db/schema.rb
@@ -22,9 +22,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_30_134445) do
     t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "winner_id"
     t.integer "home_team_id"
     t.integer "away_team_id"
+    t.integer "winner_id"
     t.index ["away_roster_id"], name: "index_matches_on_away_roster_id"
     t.index ["away_team_id"], name: "index_matches_on_away_team_id"
     t.index ["home_roster_id"], name: "index_matches_on_home_roster_id"
