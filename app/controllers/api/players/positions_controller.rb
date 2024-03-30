a188a92fbf7bf17e0a707dd7fa5cb9b707d73a26
RobertCondon <robert.condon@soeidental.com>
2024-03-31 02:08:19 +1300

    Fix zeitwork error.

--- app/controllers/api/players/positions_controller.rb
+++ app/controllers/api/players/positions_controller.rb
@@ -1,11 +1,13 @@
 module Api
-  class BackfillController < ApplicationController
+  module Players
+    class PositionsController < ApplicationController
 
-    def update
-      matches = Match.where(year: Time.now.year, round: params[:round])
-      Match.populate_current_player_positions_for_match(matches: matches)
-      render json: { message: 'Update of current positions complete' }
+      def update
+        matches = Match.where(year: Time.now.year, round: params[:round])
+        Match.populate_current_player_positions_for_match(matches: matches)
+        render json: { message: 'Update of current positions complete' }
+      end
+
     end
-
   end
 end
